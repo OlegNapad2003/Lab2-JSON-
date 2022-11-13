57e1cab5b46068c13c66b92f51f27220bb098e21
OlegNapad2003 <aleksashkin903@gmail.com>
2022-11-13 19:20:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace Lesson26
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+          
+            var groups = new List<Group>();
+            var students = new List<Student>();
+
+            for (int i = 1; i < 10; i++)
+            {
+                var group = new Group(i, "Группа " + i);
+                group.SetPrivet(i);
+                groups.Add(group);
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                var studen = new Student(Guid.NewGuid().ToString().Substring(0, 5), i % 100)
+                {
+                    Group = groups[i % 9]
+                };
+
+                students.Add(studen);
+            }
+            var jsonFormatter = new DataContractJsonSerializer(typeof(List<Student>));
+
+            using (var file = new FileStream("students.json", FileMode.Create))
+            {
+                jsonFormatter.WriteObject(file, students);
+            }
+
+            using (var file = new FileStream("students.json", FileMode.OpenOrCreate))
+            {
+                var newStudens = jsonFormatter.ReadObject(file) as List<Student>;
+
+                if (newStudens != null)
+                {
+                    foreach (var student in newStudens)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+            }
+
+             Console.ReadLine();
+
+        }
+    }
+}
